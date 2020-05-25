4422630f1befa97c9d7512f9173d4c869a43dc55
liang <46748111+yiliqiaolezi@users.noreply.github.com>
2020-05-25 13:25:01 +0800

    更新UI

--- app/src/main/java/com/deepquotes/QuotesWidgetProvider.java
+++ app/src/main/java/com/deepquotes/QuotesWidgetProvider.java
@@ -1,10 +1,12 @@
 package com.deepquotes;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import org.jetbrains.annotations.NotNull;
@@ -18,21 +20,37 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
+import static android.appwidget.AppWidgetManager.ACTION_APPWIDGET_DELETED;
+import static android.appwidget.AppWidgetManager.ACTION_APPWIDGET_UPDATE;
+
 public class QuotesWidgetProvider extends AppWidgetProvider {
 
     //窗口小部件点击时调用
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
-        Toast.makeText(context,"你点击了控件",Toast.LENGTH_SHORT).show();
-        getQuotes(context);
+        String action = intent.getAction();
+        switch (action){
+            case ACTION_APPWIDGET_UPDATE:
+                Toast.makeText(context,"你更新了控件",Toast.LENGTH_SHORT).show();
+                break;
+            case ACTION_APPWIDGET_DELETED:
+                Toast.makeText(context,"你删除了控件",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(context,"default",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        //Toast.makeText(context,"你点击了控件",Toast.LENGTH_SHORT).show();
+        //getQuotes(context);
     }
 
     //窗口小部件更新时时调用
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
-        Toast.makeText(context,"你更新了控件",Toast.LENGTH_SHORT).show();
+        //Toast.makeText(context,"你更新了控件",Toast.LENGTH_SHORT).show();
+
 
     }
 
@@ -40,21 +58,21 @@ public class QuotesWidgetProvider extends AppWidgetProvider {
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
         super.onDeleted(context, appWidgetIds);
-        Toast.makeText(context,"你删除了控件",Toast.LENGTH_SHORT).show();
+        //Toast.makeText(context,"你删除了控件",Toast.LENGTH_SHORT).show();
     }
 
     //该窗口小部件第一次添加到桌面时调用该方法
     @Override
     public void onEnabled(Context context) {
         super.onEnabled(context);
-        Toast.makeText(context,"你添加了了控件",Toast.LENGTH_SHORT).show();
+        //Toast.makeText(context,"你添加了了控件",Toast.LENGTH_SHORT).show();
     }
 
     //最后一个该窗口小部件删除时调用该方法
     @Override
     public void onDisabled(Context context) {
         super.onDisabled(context);
-        Toast.makeText(context,"你删除了最后一个控件",Toast.LENGTH_SHORT).show();
+        //Toast.makeText(context,"你删除了最后一个控件",Toast.LENGTH_SHORT).show();
     }
 
 
@@ -62,7 +80,7 @@ public class QuotesWidgetProvider extends AppWidgetProvider {
     @Override
     public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
         super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
-        Toast.makeText(context,"你改变了控件大小",Toast.LENGTH_SHORT).show();
+        //Toast.makeText(context,"你改变了控件大小",Toast.LENGTH_SHORT).show();
     }
 
 
@@ -70,7 +88,7 @@ public class QuotesWidgetProvider extends AppWidgetProvider {
     @Override
     public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
         super.onRestored(context, oldWidgetIds, newWidgetIds);
-        Toast.makeText(context,"你恢复控件",Toast.LENGTH_SHORT).show();
+        //Toast.makeText(context,"你恢复控件",Toast.LENGTH_SHORT).show();
     }
 
     public void getQuotes(final Context context){
